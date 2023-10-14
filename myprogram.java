e593c55ca6c86dd3faa21026178fb6dbfcdc60fa
saikumardumpeti <saikumar.dumpeti1919@gmail.com>
2023-10-14 14:53:10 +0530

    my Java Program

---
+++ myprogram.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ShuffleArray {
+    public static void main(String[] args) {
+        Integer[] array = {1, 2, 3, 4, 5, 6, 7};
+        List<Integer> list = Arrays.asList(array);
+        Collections.shuffle(list);
+        System.out.println("Shuffled array: " + Arrays.toString(list.toArray()));
+    }
+}
